131dd80b1cb8e89eec8ac9b31a1d39d7804f91cc
SerYuuu <sergiusggwp@gmail.com>
2026-05-08 08:57:38 +0700

    hapus sedder dumy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,19 +30,6 @@ class DatabaseSeeder extends Seeder
                 'role'     => 'admin',
             ]
         );
-
-        // 2. Gunakan withoutEvents agar Observer Audit Log tidak memicu error Integrity Constraint
-        Guest::withoutEvents(function () use ($admin) {
-            Guest::factory(50)->create([
-                'user_id' => $admin->id
-            ]);
-        });
-
-        AppGuest::withoutEvents(function () use ($admin) {
-            AppGuest::factory(50)->create([
-                'user_id' => $admin->id
-            ]);
-        });
         
         $this->command->info('Berhasil membuat user dan 100 data dummy tanpa error audit!');
     }
